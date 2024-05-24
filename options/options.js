016e106663e0505144077315188406adc374862f
Crowdbotics <team@crowdbotics.com>
2024-05-24 17:02:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "red-reach-48095",
+  url: "https://red-reach-48095.botics.co",
+  api: "https://red-reach-48095.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
